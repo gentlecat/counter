b9f8ed5e721d2775a9c2384f69f57f3e9613dfeb
Roman <roman@tsukanov.me>
2012-02-11 17:11:53 +1000

    Added sounds

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ Simple Counter is an Android application that can help you count anything. You c
 What was used
 -------------
 * [ActionBarSherlock][2] by [Jake Wharton][3]
+* Sounds from [adobeflash.com][4]
 
 
   [1]: http://tsukanov.me/stuff/simple-counter/art.png
   [2]: http://actionbarsherlock.com/
   [3]: http://jakewharton.com/
+  [4]: http://www.adobeflash.com/download/sounds/clicks/
--- src/me/tsukanov/counter/CounterFragment.java
+++ src/me/tsukanov/counter/CounterFragment.java
@@ -1,7 +1,11 @@
 package me.tsukanov.counter;
 
+import java.util.HashMap;
+
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
@@ -24,6 +28,9 @@ public class CounterFragment extends Fragment {
 	private static final int MIN_VALUE = 0;
 	private static final int DEFALUT_VALUE = MIN_VALUE;
 	private static final long DEFAULT_VIBRATION_DURATION = 30; // Milliseconds
+	private static final Integer INCREMENT_SOUND = 200;
+	private static final Integer DECREMENT_SOUND = 201;
+	private static final Integer REFRESH_SOUND = 202;
 
 	int counterValue;
 	TextView counterLabel;
@@ -31,6 +38,8 @@ public class CounterFragment extends Fragment {
 	Vibrator vibrator;
 
 	SharedPreferences settings;
+	SoundPool soundPool;
+	HashMap<Integer, Integer> soundsMap;
 
 	static CounterFragment newInstance(int id) {
 		CounterFragment fragment = new CounterFragment();
@@ -72,11 +81,22 @@ public class CounterFragment extends Fragment {
 				decrement();
 			}
 		});
+		
+		settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
 		vibrator = (Vibrator) getActivity().getSystemService(
 				Context.VIBRATOR_SERVICE);
-		settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
-
+		
+		getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
+        soundsMap = new HashMap<Integer, Integer>();
+        soundsMap.put(INCREMENT_SOUND,
+                        soundPool.load(getActivity(), R.raw.increment_sound, 1));
+        soundsMap.put(DECREMENT_SOUND,
+                        soundPool.load(getActivity(), R.raw.decrement_sound, 1));
+        soundsMap.put(REFRESH_SOUND,
+        				soundPool.load(getActivity(), R.raw.refresh_sound, 1));
+ 
 		return view;
 	}
 
@@ -100,6 +120,7 @@ public class CounterFragment extends Fragment {
 		if (counterValue < MAX_VALUE) {
 			if (settings.getBoolean("vibrationOn", true))
 				vibrator.vibrate(DEFAULT_VIBRATION_DURATION);
+			playSound(INCREMENT_SOUND);
 			counterLabel.setText(Integer.toString(++counterValue));
 			// TODO Increment in app.counters
 		}
@@ -111,6 +132,7 @@ public class CounterFragment extends Fragment {
 		if (counterValue > MIN_VALUE) {
 			if (settings.getBoolean("vibrationOn", true))
 				vibrator.vibrate(DEFAULT_VIBRATION_DURATION + 20);
+			playSound(DECREMENT_SOUND);
 			counterLabel.setText(Integer.toString(--counterValue));
 			// TODO Decrement in app.counters
 		}
@@ -121,6 +143,7 @@ public class CounterFragment extends Fragment {
 	public void refresh() {
 		if (settings.getBoolean("vibrationOn", true))
 			vibrator.vibrate(DEFAULT_VIBRATION_DURATION + 40);
+		playSound(REFRESH_SOUND);
 		counterValue = DEFALUT_VALUE;
 		counterLabel.setText(Integer.toString(counterValue));
 		// TODO Refresh in app.counters
@@ -139,5 +162,14 @@ public class CounterFragment extends Fragment {
 		else
 			decrementButton.setEnabled(true);
 	}
+	
+    private void playSound(int soundID) {
+        if (settings.getBoolean("soundsOn", false)) {
+                AudioManager audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+                float volume = (float) audioManager
+                                .getStreamVolume(AudioManager.STREAM_MUSIC);
+                soundPool.play(soundsMap.get(soundID), volume, volume, 1, 0, 1f);
+        }
+    }
 
 }
