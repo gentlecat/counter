9a6bd5082d474331bf75732ac13c21c7456f0d43
Roman Tsukanov <git@roman.zone>
2024-12-27 23:43:15 -0800

    Simplify SettingsActivity

--- app/src/main/java/me/tsukanov/counter/activities/SettingsActivity.java
+++ app/src/main/java/me/tsukanov/counter/activities/SettingsActivity.java
@@ -1,23 +1,15 @@
 package me.tsukanov.counter.activities;
 
-import android.app.AlertDialog;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceManager;
 import com.google.android.material.appbar.MaterialToolbar;
-import java.io.IOException;
-import me.tsukanov.counter.CounterApplication;
 import me.tsukanov.counter.R;
 import me.tsukanov.counter.SharedPrefKeys;
 import me.tsukanov.counter.view.SettingsFragment;
@@ -25,14 +17,6 @@ import me.tsukanov.counter.view.Themes;
 
 public class SettingsActivity extends AppCompatActivity
     implements OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
-
-  private static final String TAG = SettingsActivity.class.getSimpleName();
-
-  public static final String KEY_REMOVE_COUNTERS = "removeCounters";
-  public static final String KEY_EXPORT_COUNTERS = "exportCounters";
-  public static final String KEY_HOMEPAGE = "homepage";
-  public static final String KEY_TIP = "tip";
-  public static final String KEY_VERSION = "version";
 
   private SharedPreferences sharedPrefs;
   private SettingsFragment settingsFragment;
@@ -49,66 +33,11 @@ public class SettingsActivity extends AppCompatActivity
     sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
     Themes.initCurrentTheme(sharedPrefs);
 
-    initSettingsFragment();
-  }
-
-  private void initSettingsFragment() {
     settingsFragment = new SettingsFragment();
-    settingsFragment.setOnRemoveCountersClickListener(getOnRemoveCountersClickListener());
-    settingsFragment.setOnExportClickListener(getOnExportClickListener());
-    settingsFragment.setOnHomepageClickListener(getOnHomepageClickListener());
-    settingsFragment.setOnTipClickListener(getOnTipClickListener());
-    settingsFragment.setAppVersion(getAppVersion());
-    settingsFragment.setTheme(getCurrentThemeName());
-
     getSupportFragmentManager()
         .beginTransaction()
         .replace(R.id.settingsFrame, settingsFragment)
         .commit();
-  }
-
-  private String getCurrentThemeName() {
-    return getResources().getString(Themes.getCurrent(sharedPrefs).getLabelId());
-  }
-
-  private OnPreferenceClickListener getOnRemoveCountersClickListener() {
-    return preference -> {
-      showWipeDialog();
-      return true;
-    };
-  }
-
-  private OnPreferenceClickListener getOnExportClickListener() {
-    return preference -> {
-      try {
-        export();
-      } catch (IOException e) {
-        Log.e(TAG, "Error occurred while exporting counters", e);
-        Toast.makeText(
-                getBaseContext(),
-                getResources().getText(R.string.toast_unable_to_export),
-                Toast.LENGTH_SHORT)
-            .show();
-      }
-      return true;
-    };
-  }
-
-  private OnPreferenceClickListener getOnHomepageClickListener() {
-    return preference -> {
-      startActivity(
-          new Intent(Intent.ACTION_VIEW, Uri.parse("https://counter.roman.zone?utm_source=app")));
-      return true;
-    };
-  }
-
-  private OnPreferenceClickListener getOnTipClickListener() {
-    return preference -> {
-      startActivity(
-          new Intent(
-              Intent.ACTION_VIEW, Uri.parse("https://counter.roman.zone/tip?utm_source=app")));
-      return true;
-    };
   }
 
   @Override
@@ -118,14 +47,6 @@ public class SettingsActivity extends AppCompatActivity
       return true;
     }
     return super.onOptionsItemSelected(item);
-  }
-
-  private String getAppVersion() {
-    try {
-      return this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
-    } catch (NameNotFoundException e) {
-      return getResources().getString(R.string.unknown);
-    }
   }
 
   @Override
@@ -141,13 +62,14 @@ public class SettingsActivity extends AppCompatActivity
   }
 
   @Override
-  public boolean onPreferenceChange(final Preference preference, final Object newValue) {
+  public boolean onPreferenceChange(final @NonNull Preference preference, final Object newValue) {
     return false;
   }
 
+  @Override
   public void onSharedPreferenceChanged(
-      final SharedPreferences sharedPreferences, final String key) {
-    if (key.equals(SharedPrefKeys.THEME.getName())) {
+      final SharedPreferences sharedPreferences, final @Nullable String key) {
+    if (key != null && key.equals(SharedPrefKeys.THEME.getName())) {
       final Preference pref = settingsFragment.findPreference(SharedPrefKeys.THEME.getName());
       if (pref != null) {
         pref.setSummary(getCurrentThemeName());
@@ -156,33 +78,7 @@ public class SettingsActivity extends AppCompatActivity
     }
   }
 
-  private void showWipeDialog() {
-    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setMessage(R.string.settings_wipe_confirmation);
-    builder.setPositiveButton(
-        R.string.settings_wipe_confirmation_yes,
-        (dialog, id) -> {
-          CounterApplication.getComponent().localStorage().wipe();
-          Toast.makeText(
-                  getBaseContext(),
-                  getResources().getText(R.string.toast_wipe_success),
-                  Toast.LENGTH_SHORT)
-              .show();
-        });
-    builder.setNegativeButton(R.string.dialog_button_cancel, (dialog, id) -> dialog.dismiss());
-
-    builder.create().show();
-  }
-
-  private void export() throws IOException {
-    final Intent exportIntent = new Intent();
-    exportIntent.setAction(Intent.ACTION_SEND);
-    exportIntent.putExtra(
-        Intent.EXTRA_TEXT, CounterApplication.getComponent().localStorage().toCsv());
-    exportIntent.setType("text/csv");
-
-    final Intent shareIntent =
-        Intent.createChooser(exportIntent, getResources().getText(R.string.settings_export_title));
-    startActivity(shareIntent);
+  private String getCurrentThemeName() {
+    return getResources().getString(Themes.getCurrent(sharedPrefs).getLabelId());
   }
 }
--- app/src/main/java/me/tsukanov/counter/view/SettingsFragment.java
+++ app/src/main/java/me/tsukanov/counter/view/SettingsFragment.java
@@ -1,57 +1,33 @@
 package me.tsukanov.counter.view;
 
-import static me.tsukanov.counter.activities.SettingsActivity.KEY_EXPORT_COUNTERS;
-import static me.tsukanov.counter.activities.SettingsActivity.KEY_HOMEPAGE;
-import static me.tsukanov.counter.activities.SettingsActivity.KEY_REMOVE_COUNTERS;
-import static me.tsukanov.counter.activities.SettingsActivity.KEY_TIP;
-import static me.tsukanov.counter.activities.SettingsActivity.KEY_VERSION;
-
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import java.io.IOException;
+import me.tsukanov.counter.CounterApplication;
 import me.tsukanov.counter.R;
 import me.tsukanov.counter.SharedPrefKeys;
+import org.apache.commons.lang3.StringUtils;
 
 public final class SettingsFragment extends PreferenceFragmentCompat {
 
   private static final String TAG = SettingsFragment.class.getSimpleName();
 
-  private OnPreferenceClickListener onRemoveCountersClickListener;
-  private OnPreferenceClickListener onExportClickListener;
-  private OnPreferenceClickListener onHomepageClickListener;
-  private OnPreferenceClickListener onTipClickListener;
-
-  private String appVersion;
-  private String theme;
-
-  public void setOnRemoveCountersClickListener(
-      final @NonNull OnPreferenceClickListener onRemoveCountersClickListener) {
-    this.onRemoveCountersClickListener = onRemoveCountersClickListener;
-  }
-
-  public void setOnExportClickListener(
-      final @NonNull OnPreferenceClickListener onExportClickListener) {
-    this.onExportClickListener = onExportClickListener;
-  }
-
-  public void setOnHomepageClickListener(
-      final @NonNull OnPreferenceClickListener onHomepageClickListener) {
-    this.onHomepageClickListener = onHomepageClickListener;
-  }
-
-  public void setOnTipClickListener(final @NonNull OnPreferenceClickListener onTipClickListener) {
-    this.onTipClickListener = onTipClickListener;
-  }
-
-  public void setAppVersion(String appVersion) {
-    this.appVersion = appVersion;
-  }
-
-  public void setTheme(String theme) {
-    this.theme = theme;
-  }
+  public static final String KEY_REMOVE_COUNTERS = "removeCounters";
+  public static final String KEY_EXPORT_COUNTERS = "exportCounters";
+  public static final String KEY_HOMEPAGE = "homepage";
+  public static final String KEY_TIP = "tip";
+  public static final String KEY_VERSION = "version";
 
   @Override
   public void onCreate(final Bundle savedInstanceState) {
@@ -59,20 +35,113 @@ public final class SettingsFragment extends PreferenceFragmentCompat {
 
     try {
 
-      // Setting summaries for necessary preferences
-      findPreference(SharedPrefKeys.THEME.getName()).setSummary(theme);
-      findPreference(KEY_VERSION).setSummary(appVersion);
+      SharedPreferences sharedPrefs =
+          PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+      findPreference(SharedPrefKeys.THEME.getName())
+          .setSummary(getResources().getString(Themes.getCurrent(sharedPrefs).getLabelId()));
+
+      findPreference(KEY_VERSION).setSummary(getAppVersion());
 
       findPreference(KEY_REMOVE_COUNTERS)
-          .setOnPreferenceClickListener(onRemoveCountersClickListener);
-      findPreference(KEY_EXPORT_COUNTERS).setOnPreferenceClickListener(onExportClickListener);
+          .setOnPreferenceClickListener(getOnRemoveCountersClickListener());
+      findPreference(KEY_EXPORT_COUNTERS).setOnPreferenceClickListener(getOnExportClickListener());
 
-      findPreference(KEY_TIP).setOnPreferenceClickListener(onTipClickListener);
-      findPreference(KEY_HOMEPAGE).setOnPreferenceClickListener(onHomepageClickListener);
+      findPreference(KEY_HOMEPAGE)
+          .setOnPreferenceClickListener(
+              (Preference p) -> {
+                startActivity(
+                    new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://counter.roman.zone?utm_source=app")));
+                return true;
+              });
+
+      findPreference(KEY_TIP)
+          .setOnPreferenceClickListener(
+              (Preference p) -> {
+                startActivity(
+                    new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://counter.roman.zone/tip?utm_source=app")));
+                return true;
+              });
 
     } catch (NullPointerException e) {
       Log.e(TAG, "Unable to retrieve one of the preferences", e);
     }
+  }
+
+  @NonNull
+  private String getAppVersion() {
+    try {
+      final String versionName =
+          this.getActivity()
+              .getPackageManager()
+              .getPackageInfo(this.getActivity().getPackageName(), 0)
+              .versionName;
+      if (StringUtils.isNotEmpty(versionName)) {
+        return versionName;
+      } else {
+        return getResources().getString(R.string.unknown_version);
+      }
+    } catch (NullPointerException | PackageManager.NameNotFoundException e) {
+      return getResources().getString(R.string.unknown_version);
+    }
+  }
+
+  @NonNull
+  private OnPreferenceClickListener getOnRemoveCountersClickListener() {
+    return preference -> {
+      showWipeDialog();
+      return true;
+    };
+  }
+
+  @NonNull
+  private OnPreferenceClickListener getOnExportClickListener() {
+    return preference -> {
+      try {
+        export();
+      } catch (IOException e) {
+        Log.e(TAG, "Error occurred while exporting counters", e);
+        Toast.makeText(
+                this.getActivity(),
+                getResources().getText(R.string.toast_unable_to_export),
+                Toast.LENGTH_SHORT)
+            .show();
+      }
+      return true;
+    };
+  }
+
+  private void showWipeDialog() {
+    final AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
+    builder.setMessage(R.string.settings_wipe_confirmation);
+    builder.setPositiveButton(
+        R.string.settings_wipe_confirmation_yes,
+        (dialog, id) -> {
+          CounterApplication.getComponent().localStorage().wipe();
+          Toast.makeText(
+                  this.getActivity(),
+                  getResources().getText(R.string.toast_wipe_success),
+                  Toast.LENGTH_SHORT)
+              .show();
+        });
+    builder.setNegativeButton(R.string.dialog_button_cancel, (dialog, id) -> dialog.dismiss());
+
+    builder.create().show();
+  }
+
+  private void export() throws IOException {
+    final Intent exportIntent = new Intent();
+    exportIntent.setAction(Intent.ACTION_SEND);
+    exportIntent.putExtra(
+        Intent.EXTRA_TEXT, CounterApplication.getComponent().localStorage().toCsv());
+    exportIntent.setType("text/csv");
+
+    final Intent shareIntent =
+        Intent.createChooser(exportIntent, getResources().getText(R.string.settings_export_title));
+    startActivity(shareIntent);
   }
 
   @Override
