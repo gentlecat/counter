98af3be8bf638bc0b16c2e0b9e03eb7c403d938c
Roman Tsukanov <git@roman.zone>
2024-12-30 23:35:54 -0800

    Hide control buttons if the app hasn't been used yet

--- app/src/main/java/me/tsukanov/counter/CounterApplication.kt
+++ app/src/main/java/me/tsukanov/counter/CounterApplication.kt
@@ -2,6 +2,7 @@ package me.tsukanov.counter
 
 import android.app.Activity
 import android.app.Application
+import androidx.preference.PreferenceManager
 import com.google.android.material.color.DynamicColors
 import dagger.android.DispatchingAndroidInjector
 import me.tsukanov.counter.domain.CounterApplicationComponent
@@ -11,6 +12,13 @@ import javax.inject.Inject
 
 class CounterApplication : Application() {
 
+    companion object {
+        var PACKAGE_NAME: String? = null
+
+        var component: CounterApplicationComponent? = null
+            private set
+    }
+
     @JvmField
     @Inject
     var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>? = null
@@ -18,20 +26,25 @@ class CounterApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        DynamicColors.applyToActivitiesIfAvailable(this)
-
         component = DaggerCounterApplicationComponent.builder()
             .counterModule(CounterModule(this))
             .build()
 
+        initDefaultPreferences()
+
         PACKAGE_NAME = applicationContext.packageName
+
+        DynamicColors.applyToActivitiesIfAvailable(this)
     }
 
-    companion object {
-        var PACKAGE_NAME: String? = null
-
-        var component: CounterApplicationComponent? = null
-            private set
+    private fun initDefaultPreferences() {
+        val wasAppUsed = component!!.localStorage()!!.readAll(false).isNotEmpty()
+        if (!wasAppUsed) {
+            // Hide controls if this this is the first time the app is used.
+            PreferenceManager.getDefaultSharedPreferences(this).edit()
+                .putBoolean(SharedPrefKeys.HIDE_CONTROLS.key, true)
+                .commit()
+        }
     }
 
 }
