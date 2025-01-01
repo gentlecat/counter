782414b9e2e267ffe1857a157afe6b078a69f8e0
Roman Tsukanov <git@roman.zone>
2024-12-31 16:36:03 -0800

    Set up first use tutorial

--- app/src/main/java/me/tsukanov/counter/CounterApplication.kt
+++ app/src/main/java/me/tsukanov/counter/CounterApplication.kt
@@ -2,9 +2,14 @@ package me.tsukanov.counter
 
 import android.app.Activity
 import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceManager
 import com.google.android.material.color.DynamicColors
 import dagger.android.DispatchingAndroidInjector
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import me.tsukanov.counter.domain.CounterApplicationComponent
 import me.tsukanov.counter.domain.CounterModule
 import me.tsukanov.counter.domain.DaggerCounterApplicationComponent
@@ -30,20 +35,28 @@ class CounterApplication : Application() {
             .counterModule(CounterModule(this))
             .build()
 
-        initDefaultPreferences()
+        initFirstUse()
 
         PACKAGE_NAME = applicationContext.packageName
 
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 
-    private fun initDefaultPreferences() {
+    private fun initFirstUse() {
         val wasAppUsed = component!!.localStorage()!!.readAll(false).isNotEmpty()
         if (!wasAppUsed) {
+
             // Hide controls if this this is the first time the app is used.
             PreferenceManager.getDefaultSharedPreferences(this).edit()
                 .putBoolean(SharedPrefKeys.HIDE_CONTROLS.key, true)
                 .commit()
+
+            // Show a tutorial toast after a few seconds.
+            ProcessLifecycleOwner.get().lifecycleScope.launch {
+                delay(3_000)
+                Toast.makeText(this@CounterApplication, R.string.toast_tutorial, Toast.LENGTH_LONG).show()
+            }
+
         }
     }
 
